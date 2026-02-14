9ab7a1b5eff4693914774640ba4199bdd3decd44
nithish252004 <nithishkarthikofficial@gmail.com>
2026-02-14 14:33:07 +0000

    Completed Module 4 - Extract required Wireshark parameters

--- Module-4/extract_files.sh
+++ Module-4/extract_files.sh
@@ -10,29 +10,29 @@ OUTPUT_FILE="output.txt"
 
 
 if [ $# -ne 1 ]; then
-	echo "Give only one Argument after the file name"
-	exit 1
+        echo "Give only one Argument after the file name"
+        exit 1
 fi
 
 if [ ! -f "$INPUT_FILE" ]; then
-	echo "Input File Does'nt Exist"
-	exit 1
+        echo "Input File Does'nt Exist"
+        exit 1
 fi
 COUNT=0
 
 while read -r line
 do
-	COUNT=$((COUNT+1))
-	if [ $COUNT -gt 50 ]; then
-		break
-	fi
+        COUNT=$((COUNT+1))
+        if [ $COUNT -gt 50 ]; then
+                break
+        fi
 
-	if echo "$line" | grep -q '"frame.time"' ||\
-	   echo "$line" | grep -q '"wlan.fc.type"' ||\
-	   echo "$line" | grep -q '"wlan.fc.subtype"'
-	then 
-	   echo "$line" >> "$OUTPUT_FILE"
-	fi
+        if echo "$line" | grep -q '"frame.time"' ||\
+           echo "$line" | grep -q '"wlan.fc.type"' ||\
+           echo "$line" | grep -q '"wlan.fc.subtype"'
+        then
+           echo "$line" >> "$OUTPUT_FILE"
+        fi
 
 
 done < "$INPUT_FILE"
